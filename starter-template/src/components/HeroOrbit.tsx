7b011c02490c8d3bc5ca7389d72962d1fc203d42
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-20 02:31:03 +0530

    rings stars and nav z index done

---
+++ starter-template/src/components/HeroOrbit.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren } from "react";
+
+const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div
+        className="flex items-start "
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          height: `${size}px`,
+          width: `${size}px`,
+        }}
+      >
+        <div
+          className="inline-flex "
+          style={{
+            transform: `rotate(${rotation * -1}deg )`,
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroOrbit;
--- starter-template/src/sections/Header.tsx
+++ starter-template/src/sections/Header.tsx
@@ -1,13 +1,13 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center relative top-3">
+    <div className="flex justify-center items-center fixed z-10 w-full top-3">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a
           className="nav-item"
           href="#"
           target="_blank"
           rel="noopener noreferrer"
-        > 
+        >
           Home
         </a>
         <a
