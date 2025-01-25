f7395dfc99847e682ca5ff77f861c939e3a82674
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-26 02:16:23 +0530

    location map view set

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -146,9 +146,15 @@ export const AboutSection = () => {
               ))}
             </div>
           </Card>
-          <Card>
-            <Image src={mapImage} alt="location" />
-            <Image src={smileEmoji} alt="smiling me" />
+          <Card className="h-[320px] p-0 relative">
+            <Image
+              src={mapImage}
+              alt="location"
+              className="h-full w-full object-cover object-left-top"
+            />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-yellow-300 to-green-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <Image src={smileEmoji} alt="smiling me" className="size-20" />
+            </div>
           </Card>
         </div>
       </div>
