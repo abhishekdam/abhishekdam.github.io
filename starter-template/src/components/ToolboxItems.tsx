0abc49a7e80b736fdd575c2220e8d0227640050e
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-26 02:03:31 +0530

    till toolbox done

---
+++ starter-template/src/components/ToolboxItems.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import TechIcon from "./TechIcon";
+import { twMerge } from "tailwind-merge";
+
+const ToolboxItems = ({
+  items,
+  className,
+  itemsWrapperClassName,
+}: {
+  className?: string;
+  itemsWrapperClassName?: string;
+  items: {
+    title: string;
+    iconType: React.ElementType;
+  }[];
+}) => {
+  return (
+    <div
+      className={twMerge(
+        "flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]",
+        className
+      )}
+    >
+      <div
+        className={twMerge(
+          "flex flex-none py-0.5 gap-6 pr-6",
+          itemsWrapperClassName
+        )}
+      >
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg "
+          >
+            <TechIcon component={item.iconType} />
+            <span className="font-semibold">{item.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ToolboxItems;
--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -1,3 +1,152 @@
+import Card from "@/components/Card";
+import SectionHeader from "@/components/SectionHeader";
+import StarIcon from "@/assets/icons/star.svg";
+import Image from "next/image";
+import bookImage from "@/assets/images/book-cover.png";
+import JSicon from "@/assets/icons/square-js.svg";
+import HtmlIcon from "@/assets/icons/html5.svg";
+import Css3Icon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GitIcon from "@/assets/icons/github.svg";
+import TechIcon from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import smileEmoji from "@/assets/images/memoji-smile.png";
+import Cardheader from "@/components/Cardheader";
+import ToolboxItems from "@/components/ToolboxItems";
+
+const toolBoxItems = [
+  {
+    title: "JavaScript",
+    iconType: JSicon,
+  },
+  {
+    title: "HTML5",
+    iconType: HtmlIcon,
+  },
+  {
+    title: "CSS3",
+    iconType: Css3Icon,
+  },
+  {
+    title: "ReactJS",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Git",
+    iconType: GitIcon,
+  },
+];
+
+const hobbies = [
+  {
+    title: "painting",
+    emoji: "🎨",
+    left: "50%",
+    top: "50%",
+  },
+  {
+    title: "Photography",
+    emoji: "📸",
+    left: "",
+    top: "",
+  },
+  {
+    title: "Gaming",
+    emoji: "🏏",
+    left: "",
+    top: "",
+  },
+  {
+    title: "Hiking",
+    emoji: "🥾",
+    left: "",
+    top: "",
+  },
+  {
+    title: "Music",
+    emoji: "📻",
+    left: "",
+    top: "",
+  },
+  {
+    title: "Fitness",
+    emoji: "🏋️",
+    left: "",
+    top: "",
+  },
+  {
+    title: "Reading",
+    emoji: "📖",
+    left: "",
+    top: "",
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, what i do, and what inspires me."
+        />
+        <div className="mt-20 flex flex-col gap-8">
+          <Card className="h-[320px]">
+            <Cardheader
+              title="My reads"
+              description="Explore the books shaping my perspectives"
+            />
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="book cover" />
+            </div>
+          </Card>
+          <Card className="h-[320px] p-0">
+            <Cardheader
+              title="My Toolbox"
+              description="Explore the technologies and tools I use to create digital
+                experiences."
+              className="px-6 pt-6"
+            />
+
+            <ToolboxItems items={toolBoxItems} className="mt-6" />
+            <ToolboxItems
+              items={toolBoxItems}
+              className="mt-6"
+              itemsWrapperClassName="-translate-x-1/2"
+            />
+          </Card>
+          <Card>
+            <Cardheader
+              title="Beyond the code"
+              description="Explore my interests and hobbies beyond there"
+            />
+
+            <div>
+              {hobbies.map((hobbi) => (
+                <div
+                  key={hobbi.title}
+                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-yellow-300 to-green-400 rounded-full py-1.5"
+                >
+                  <span className="font-medium text-gray-950">
+                    {hobbi.title}
+                  </span>
+                  <span>{hobbi.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <Image src={mapImage} alt="location" />
+            <Image src={smileEmoji} alt="smiling me" />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
