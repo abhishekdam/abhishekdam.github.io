ab7ecd91e7d7a5fe4f2daaffc3f0378cb0dedfe8
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-26 13:16:37 +0530

    mobile footer done

--- starter-template/src/sections/Footer.tsx
+++ starter-template/src/sections/Footer.tsx
@@ -21,7 +21,8 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-yellow-300/40 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
           <div className="text-white/40">&copy; 2025. All rights reserved</div>
