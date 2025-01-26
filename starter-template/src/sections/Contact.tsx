1a6a1f3f4758a6246414a6bac2c9ee1f78980ccb
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-26 12:43:22 +0530

    call to action done

--- starter-template/src/sections/Contact.tsx
+++ starter-template/src/sections/Contact.tsx
@@ -3,7 +3,7 @@ import grainImage from "@/assets/images/grain.jpg";
 
 export const ContactSection = () => {
   return (
-    <div className="py-16 pt-12">
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
       <div className="container">
         <div className="bg-gradient-to-r from-yellow-300 to-green-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
           <div
