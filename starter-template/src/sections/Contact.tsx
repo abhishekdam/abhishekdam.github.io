8736a7a49dd0cce9cefc0c4a2af11121bd92701d
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-26 13:08:59 +0530

    footter done skeleton

--- starter-template/src/sections/Contact.tsx
+++ starter-template/src/sections/Contact.tsx
@@ -15,10 +15,10 @@ export const ContactSection = () => {
           <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create somthing amazing together
+                Let&apos;s create somthing amazing together
               </h2>
               <p className="text-sm md:text-base mt-2">
-                Ready to brgin your next project to life? Let's connect and
+                Ready to brgin your next project to life? Let&apos;s connect and
                 discuss how I can help you achieve your goals
               </p>
             </div>
--- starter-template/src/sections/Footer.tsx
+++ starter-template/src/sections/Footer.tsx
@@ -1,3 +1,44 @@
+import ArrowUpRighIcon from "@/assets/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  {
+    title: "Youtube",
+    href: "#",
+  },
+  {
+    title: "Twitter",
+    href: "#",
+  },
+  {
+    title: "Instagram",
+    href: "#",
+  },
+  {
+    title: "LinkedIn",
+    href: "#",
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
+          <div className="text-white/40">&copy; 2025. All rights reserved</div>
+          <nav className="flex flex-col items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                href="#"
+                key={link.title}
+                className="inline-flex items-center gap-1.5 "
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRighIcon className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
