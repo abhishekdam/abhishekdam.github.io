ab9a8a645023f185d4ef395cf0bf1c174a547561
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-27 20:21:07 +0530

    updateoing

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   output: "export",
   webpack(config) {
     // Grab the existing rule that handles SVG imports
